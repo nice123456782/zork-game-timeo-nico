a824752b34b4f06196beca5bc39419fbe4e4ffd1
nicoweiss1 <148877618+nicoweiss1@users.noreply.github.com>
2025-05-08 08:50:40 +0200

    added Items

---
+++ src/main/java/ch/bbw/zork/Item.java
@@ -0,0 +1,25 @@
+package ch.bbw.zork;
+
+public class Item {
+    private String name; // Privates String Attribut namens "name" für den Namen des Items
+    private String description; // Privates String Attribut namens "description" für die Beschreibung des Items
+
+    public Item(String name, String description) { // Konstruktor der Public Class Item wird verwendet wenn man die Klasse aufruft | z.B Item schlüssel = new Item("Schlüssel", "Ein alter rostiger Schlüssel"); | "Schlüssel" wird in name gespeichert und man muss halt sagen name ist ein string
+        this.name = name; // Eingebener Name wird in this.name gespeichert also "name" gespeichert
+        this.description = description; // Eingegebene Beschreibung wird in this.description also "description" gespeichert
+    }
+
+    public String getName() { // Mit dieser Getter Methode kann man von ausserhalb dieser Klasse den Namen abfragen halt nicht direkt weil ja name private ist darum diese getter methode
+        return name; // name wird zurückgegeben
+    }
+
+    public String getDescription() { // Mit dieser Getter Methode kann mann von ausseralbt dieser Klasse die Beschreiben abfragen halt wieder nicht direkt weil ja descripition privat ist
+        return description; // beschreibung wird zurückgegeben
+    }
+
+    @Override // Man überschreibt die Methode toString aus der Klasse Object
+    public String toString() { // öffentliche toString Methode, die ein String zurückgibt
+        return name + ": " + description; // return den namen: beschreibung
+    } // Diese to String Methode ist wichtig und wir immer nur bei Textaufrufen verwendet bsp: Item schlüssel = new Item("Schlüssel", "Ein alter Schlüssel");
+        //System.out.println(schlüssel); dann wird diese toString Methode verwendet
+}
