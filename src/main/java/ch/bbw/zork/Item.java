f8f9d9f27ae677aa086b5f1f5779c68995f893af
nicoweiss1 <148877618+nicoweiss1@users.noreply.github.com>
2025-05-12 11:03:27 +0200

    Viele neue Funktionen

--- src/main/java/ch/bbw/zork/Item.java
+++ src/main/java/ch/bbw/zork/Item.java
@@ -4,13 +4,19 @@ public class Item {
     private String name; // Privates String Attribut namens "name" für den Namen des Items
     private String description; // Privates String Attribut namens "description" für die Beschreibung des Items
 
-    public Item(String name, String description) { // Konstruktor der Public Class Item wird verwendet wenn man die Klasse aufruft | z.B Item schlüssel = new Item("Schlüssel", "Ein alter rostiger Schlüssel"); | "Schlüssel" wird in name gespeichert und man muss halt sagen name ist ein string
+    private int weight; // Privates integer Attribut names weight
+    public Item(String name, String description, int weight) { // Konstruktor der Public Class Item wird verwendet wenn man die Klasse aufruft | z.B Item schlüssel = new Item("Schlüssel", "Ein alter rostiger Schlüssel"); | "Schlüssel" wird in name gespeichert und man muss halt sagen name ist ein string
         this.name = name; // Eingebener Name wird in this.name gespeichert also "name" gespeichert
         this.description = description; // Eingegebene Beschreibung wird in this.description also "description" gespeichert
+        this.weight = weight; // Eingegebenes Gewicht "weight" wird in this.weight also "weight" Attribut gespeichert.
     }
 
     public String getName() { // Mit dieser Getter Methode kann man von ausserhalb dieser Klasse den Namen abfragen halt nicht direkt weil ja name private ist darum diese getter methode
         return name; // name wird zurückgegeben
+    }
+
+    public int getWeight() { // Mit cieser Getter Methoder kann man von ausserhalb dieser Klasse das Gewicht abfragen halt nicht direkt weil ja weight attribut privat ist darum diese getter methode
+        return weight; // weight attribut wird zurückgegeben
     }
 
     public String getDescription() { // Mit dieser Getter Methode kann mann von ausseralbt dieser Klasse die Beschreiben abfragen halt wieder nicht direkt weil ja descripition privat ist
@@ -19,7 +25,7 @@ public class Item {
 
     @Override // Man überschreibt die Methode toString aus der Klasse Object
     public String toString() { // öffentliche toString Methode, die ein String zurückgibt
-        return name + ": " + description; // return den namen: beschreibung
+        return name + ": " + description + " (Gewicht: " + weight + ")"; // return den namen: beschreibung
     } // Diese to String Methode ist wichtig und wir immer nur bei Textaufrufen verwendet bsp: Item schlüssel = new Item("Schlüssel", "Ein alter Schlüssel");
         //System.out.println(schlüssel); dann wird diese toString Methode verwendet
 }
---
+++ src/main/java/ch/bbw/zork/Player.java
@@ -0,0 +1,35 @@
+package ch.bbw.zork;
+
+
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Item> inventory = new ArrayList<>(); // Variable namens inventory Typ ist eine Arrayliste die Item Objekte speichern kann | Ist privat und erstellt eine neue Arrayliste in der man Item Objekte speichern kann.
+    private int maxWeight = 10;
+    private int currentWeight = 0;
+
+    public void takeItem(Item item) { // Public Methode ohne Rückgabe| nimmt ein objekt "item" von der Klasse oder Typ genannt Item als Parameter
+        if (currentWeight + item.getWeight() > maxWeight) { // Wenn das aktuelle Gewicht + das gewicht des Items grösser ist also das maximal gewicht
+            System.out.println("❌ Du kannst " + item.getName() + " nicht aufnehmen – zu schwer!");
+        }
+        else { // Wann das so nicht stimmt beim if dann:
+            inventory.add(item); // das "item" Objekt wird hinzugefügt zu der ArrayListe "inventory", übrigens bei array listen immer ".add"
+            currentWeight += item.getWeight(); // currentWeight wird geändert zu einem neuen Gewicht also currentWeight = currentWeight + item.getWeight also das Gewicht des Items
+            System.out.println("Du hast \"" + item.getName() + "\" aufgenommen."); // Diese TextAusgabe | "getName()" eine Methode aus der Item Klasse | der Backslash also \ steht für einen Zeilenumbruch oder Abstand
+        }
+    }
+
+    public String showInventory() { // Public Methode | Gibt einen String zurück | Namens "showInventory"
+        if (inventory.isEmpty()) { // Wenn die Array Liste "inventory" leer ist dann:
+            return "Dein Inventar ist leer."; // Diese Rückgabe als String
+        }
+        StringBuilder builder = new StringBuilder("Dein Inventar:\n"); // Stringbuilder ist eien Java Klasse mit der man mehrere Stringszusammensetzen kann | Also builder ist vom Typ StringBuilder | builder hat den Wert von einem neuen Stringbuilder mit bis jetzt einem kleinen Text und Zeilenabstand
+        for (Item item : inventory) { // Für jedes Objekt "item" von der Klasse/Typ "Item" im inventory also der Arrayliste:
+            builder.append("- ").append(item).append("\n"); // Zu dem StringBuilder "builder" wird hinzugefügt das hier also z.B - schluessel  dann Zeilenumbruch
+        }
+        builder.append("Gesamtgewicht: ").append(currentWeight).append(" /").append(maxWeight).append(" kg\n"); // Das wird ebenfalls noch zum StringBuilder "builder" hinnzugefügt sieht dann so aus beispielsweise -> Gesamtgewicht: 2 / 5 kg
+        return builder.toString(); // Ganzer zusammengesetzter String also der "builder" zu einem String zurückgeben
+    }
+
+
+}
